6b01573b0e86827462c9d8751add3eed5d74d166
Molly Oretsky <mollyoretsky@gmail.com>
2018-06-29 23:38:14 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,5 +1,32 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', function() {
+    alert('hey!')
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt(){
+  $("form").on("keydown", 
+    function(key){
+      if(key.which == 71){
++        alert("g was pressed")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(e) {
+    alert('Your form is going to be submitted now.')
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
